bbe8a26397b2a9b29328add1c8a590ffa8724533
Michiel Dral <m.c.dral@gmail.com>
2022-01-19 22:26:34 +0100

    Other tags

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import {
 } from "@codemirror/language";
 import type { Extension } from "@codemirror/state";
 import * as autocomplete from "@codemirror/autocomplete";
-import * as highlight from "@codemirror/highlight";
+import { styleTags, tags as t } from "@codemirror/highlight";
 import * as indent from "./indent";
 
 type SyntaxConfig = {
@@ -47,34 +47,67 @@ function getSyntaxConfig(): SyntaxConfig {
 
 let syntaxConfig = getSyntaxConfig();
 
-let styleTags = highlight.styleTags({
-  String: highlight.tags.string,
-  TripleString: highlight.tags.string,
-  CommandString: highlight.tags.string,
-  StringWithoutInterpolation: highlight.tags.string,
-  TripleStringWithoutInterpolation: highlight.tags.string,
-  CommandStringWithoutInterpolation: highlight.tags.string,
+console.log(
+  `syntaxConfig.keywords.map((t) => t.name):`,
+  syntaxConfig.keywords.map((t) => t.name)
+);
 
-  Comment: highlight.tags.lineComment,
-  BlockComment: highlight.tags.comment,
-  [syntaxConfig.keywords.map((t) => t.name).join(" ")]: highlight.tags.keyword,
-  "( )": highlight.tags.paren,
-  "[ ]": highlight.tags.paren,
-  "{ }": highlight.tags.paren,
+let juliaStyleTags = styleTags({
+  String: t.string,
+  TripleString: t.string,
+  CommandString: t.string,
+  StringWithoutInterpolation: t.string,
+  TripleStringWithoutInterpolation: t.string,
+  CommandStringWithoutInterpolation: t.string,
 
-  BooleanLiteral: highlight.tags.bool,
-  Number: highlight.tags.number,
+  Comment: t.lineComment,
+  BlockComment: t.comment,
 
-  Identifier: highlight.tags.variableName,
-  "MacroIdentifier! MacroFieldExpression!": highlight.tags.macroName,
-  "FieldName!": highlight.tags.propertyName,
-  "Symbol!": highlight.tags.atom,
+  "mutable struct StructDefinition/end": t.definitionKeyword,
+  "primitive type PrimitiveDefinition/end": t.definitionKeyword,
+  "const local global": t.definitionKeyword,
+  "module ModuleDefinition/end import using export": t.moduleKeyword,
+
+  "ForStatement/for ForBinding/in ForStatement/end": t.controlKeyword,
+  "WhileStatement/while WhileStatement/end": t.controlKeyword,
+  "IfClause/if IfClause/elseif ElseClause/else IfStatement/end":
+    t.controlKeyword,
+  "default break return": t.controlKeyword,
+  "TryStatement/try CatchClause/catch FinallyClause/finally TryStatement/end":
+    t.controlKeyword,
+
+  AssignmentExpression: t.propertyName,
+
+  ":: <:": t.typeOperator,
+
+  "( )": t.paren,
+  "[ ]": t.paren,
+  "{ }": t.paren,
+  [syntaxConfig.keywords.map((t) => t.name).join(" ")]: t.keyword,
+
+  BooleanLiteral: t.bool,
+  Number: t.number,
+  // "CoefficientExpression!": t.special(t.number),
+  "Coefficient!": t.unit,
+
+  Identifier: t.variableName,
+  "MacroIdentifier! MacroFieldExpression!": t.macroName,
+  "FieldName!": t.propertyName,
+  FieldExpression: t.propertyName,
+  "FieldExpression .": t.derefOperator,
+  "Symbol!": t.atom,
+
+  // "get set async static": t.modifier,
+  // "for while do if else switch try catch finally return throw break continue default case": t.controlKeyword,
+  // "in of await yield void typeof delete instanceof": t.operatorKeyword,
+  // "import export from": t.moduleKeyword,
+  // "with debugger as new": t.keyword,
 });
 
 let language = LRLanguage.define({
   parser: parser.configure({
     props: [
-      styleTags,
+      juliaStyleTags,
       indentNodeProp.add({
         ...syntaxConfig.indents,
         ModuleDefinition: indent.noIndent,
